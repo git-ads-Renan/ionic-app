ff8860088909410f00bed5a76743ee6a4904b76b
git-ads-Renan <189377@upf.br>
2021-10-11 21:01:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SkateApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
